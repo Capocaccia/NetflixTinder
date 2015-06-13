4f12a771de50eca2ff3ed5cc7ab9ae6d6038ed20
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-06-13 11:06:28 -0500

    Working tinder for movies app

---
+++ candyflix/app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular
+  .module('candyflixAngularApp', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+
+      .when('/', {
+        templateUrl: 'views/movies.html',
+        controller: 'MoviesCtrl'
+      })
+      .when('/watchlist', {
+        templateUrl: 'views/watchlist.html',
+        controller: 'WatchlistCtrl'
+      })
+
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
